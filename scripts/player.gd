1d2f2db9306b799dd04b7feb1a7d0b66d74e97ee
Theo Wiik <theo.le.wiik@gmail.com>
2025-12-13 00:18:17 +0100

    Camera bobbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,13 +9,18 @@ var jump_velocity: float = 4.5
 var gravity: float = 9.82
 var speed: float = 5.0
 
-var rotation_smoothing: float = 8.0
-var camera_position_smoothing: float = 10.0
-var camera_rotation_smoothing: float = 10.0
+var rotation_smoothing: float = 5.0
+var camera_position_smoothing: float = 15.0
+var camera_rotation_smoothing: float = 15.0
 
 var target_rotation_y: float = 0.0
 var target_rotation_x: float = 0.0
 var current_rotation_x: float = 0.0
+
+var bob_time: float = 0.0
+var bob_speed: float = 12.0
+var bob_intensity: float = 0.08
+var bob_sway: float = 0.05
 
 
 func _ready() -> void:
@@ -25,6 +30,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	move(delta)
 	smooth_rotation(delta)
+	apply_camera_bob(delta)
 	smooth_camera(delta)
 
 
@@ -36,13 +42,29 @@ func _unhandled_input(event: InputEvent) -> void:
 		var mouse_delta: Vector2 = event.relative
 		target_rotation_y += deg_to_rad(-mouse_delta.x * 0.1)
 		target_rotation_x += deg_to_rad(-mouse_delta.y * 0.1)
-		target_rotation_x = clamp(target_rotation_x, deg_to_rad(-89), deg_to_rad(89))
+		target_rotation_x = clamp(target_rotation_x, deg_to_rad(-60), deg_to_rad(60))
 
 
 func smooth_rotation(delta: float) -> void:
 	rotation.y = lerp_angle(rotation.y, target_rotation_y, rotation_smoothing * delta)
 	current_rotation_x = lerp(current_rotation_x, target_rotation_x, rotation_smoothing * delta)
 	view_pivot.rotation.x = current_rotation_x
+
+
+func apply_camera_bob(delta: float) -> void:
+	var velocity_2d := Vector2(velocity.x, velocity.z)
+	var is_moving := velocity_2d.length() > 0.1 and is_on_floor()
+
+	if is_moving:
+		bob_time += delta * bob_speed
+	else:
+		bob_time = lerp(bob_time, 0.0, delta * 10.0)
+
+	var bob_offset_y := sin(bob_time) * bob_intensity
+	var bob_offset_x := cos(bob_time * 0.5) * bob_sway
+
+	view_pivot.position.y = bob_offset_y
+	view_pivot.position.x = bob_offset_x
 
 
 func smooth_camera(delta: float) -> void:
